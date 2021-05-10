6d690b972d911d2274eb732329745b34cb7dfd7b
SylviaZiyuZhang <ziyuzhan@andrew.cmu.edu>
2021-05-10 13:28:26 -0400

    Renamed 2D testcases and added checker script

--- 2Drunner.cpp
+++ 2Drunner.cpp
@@ -27,5 +27,31 @@ int main(int argc, char** argv)
     //    cout << "Output size: " << hull_size << std::endl;
     //    printPoints2D(hull, hull_size);
     //}
+
+    /*
+    std::unordered_set<point2D> hull_set = std::unordered_set<point2D>();
+    for (int i = 0; i < hull_size; i++) {
+      hull_set.insert(hull[i]);
+    }
+    point2D *par_hull = new point2D[size];
+    auto par_start = std::chrono:high_resolution_clock::now();
+    int par_hull_size = Parallel2DCH:convexHull2D(points, size, par_hull, debug);
+    auto par_stop = std::chrono:high_resolution_clock::now();
+    auto par_duration = std::chrono::duration_cast<std::chrono::microseconds>(par_stop - par_start);
+    std::cout << "Parallel execution on size " << size << " dataset took " << par_duration.count() << " microseconds" << std::endl;
+    bool correct = true;
+    if (par_hull_size != hull_size) {
+      correct = false;
+    } else {
+      for (int i = 0; i < par_hull_size; i++) {
+        if (hull_set.find(par_hull[i]) == hull_set.end()) {
+          correct = false;
+        }
+      }
+    }
+    if (!correct) {
+      std::cout << "Parallel solution INCORRECT" << std::endl;
+    }
+    */
     return 0;
 }
